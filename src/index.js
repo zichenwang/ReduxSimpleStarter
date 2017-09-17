44069c3fa1f4c44d490164d02ee7246ca133d91b
zichenwang <wangzichen530@hotmail.com>
2017-09-17 12:42:59 -0500

    - 28

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
-import SearchBar from './components/search_bar'
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyASyELXBkJA0XNdDrCRZ32VeK4_FK1J6h0';
 
@@ -21,6 +23,8 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
+        <VideoList videos={this.state.videos} />
       </div>
     )
   }
